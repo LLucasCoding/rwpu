c94553cdf63a66120925e70e3ebe34a477b8834f
Lucas <lukipuki.3.14159@gmail.com>
2025-03-21 13:51:08 -0600

    Formatting, bug fixes, tested with .rwpu files

--- RWPU-Assembler.py
+++ RWPU-Assembler.py
@@ -6,7 +6,7 @@ except FileNotFoundError:
     exit(0)
 lines = f.readlines()
 
-warnlevel = 2 # 0 = silent except at end if error, 1 = errors only, 2 = errors, warnings, 3 = logs, errors, warnings, 4 = verbose
+warnlevel = 4 # 0 = silent except at end if error, 1 = errors only, 2 = errors, warnings, 3 = logs, errors, warnings, 4 = verbose; 2 = default
 
 instruction = 1 # Position of current instruction
 instructionbinary = "" # Binary value for that instruction, which gets pushed to instruction list when it is done.
@@ -17,6 +17,7 @@ stackdepth = 0 # To track stack overflow
 baseref = list("0123456789ABCDEFGHIJKLMNOPQRSTUV") # All characters for bases up to 32
 line = 0 # Line, to tell the user what line to look at when an error occurs
 error = False # Error variable, doesn't write to file or display final code result if there is a fatal error.
+compilesuccess = True # If a line was compiled properly
 
 def msg(m, level):
     formatter = ""
@@ -25,7 +26,7 @@ def msg(m, level):
         formatter = "\033[031m"
     elif level == 2:
         formatter = "\033[033m"
-    if level >= warnlevel:
+    if level <= warnlevel:
         print("{}{}\033[0m".format(formatter, m))
 
 def format(n, l): # format integer n to have l places minimum
@@ -94,15 +95,20 @@ def log2int(integer): # log2(n) rounded down
         mask <<= 1
     return result
 
+if filename.split(".")[-1] != "rwpu" or len(filename.split(".")) == 1:
+    msg("Warning: Filename specified doesn't end in .rwpu, which is the proper file extension", 2)
+
 for i in lines:
+    compilesuccess = True
     line += 1
-    msg("Starting new line", 4)
+    msg("Starting new line ({})".format(line), 4)
     instructionbinary = ""
     islabel = 0
     if list(i)[0] == "/": # line comment, (line starts with /)
         msg("Comment on line {}".format(line), 4)
         continue
     brokeninst = i.split(" ")
+    msg("Broken instruction: {}".format(brokeninst), 4)
     if list(i)[0] == ".": # if the line is a label
         msg("Label found on line", 4)
         labelnames.append(lines.split(" ")[0])
@@ -116,13 +122,14 @@ for i in lines:
     if op == "add":
         if len(brokeninst)-islabel != 4:
             error = True
-            msg("\033[031mInstructionLengthError: Operation add has 3 parameters, {} given.\n{}\nLine {} in file {}\n\033[01m".format(len(brokeninst)-1-islabel, i, line, filename), 1)
+            msg("\033[031mInstructionLengthError: Operation add has 3 parameters, {} arguments given.\n{}Line {} in file {}\n\033[01m".format(len(brokeninst)-1-islabel, i, line, filename), 1)
             continue
         instructionbinary = "0001" # ADD opcode
-        for j in range(islabel, len(brokeninst)):
+        for j in range(islabel+1, len(brokeninst)):
             if list(brokeninst[j])[0] != "r" and list(brokeninst[j]) != "$":
                 error = True
-                msg("\033[031mArgumentError: Argument {} in add instruction is not a register. ADD parameters are ADD regA regB regOut.\nValid prefixes for register argument: r $\n{}\nLine {} in file {}\n\033[01m".format(brokeninst[j], i, line, filename), 1)
+                msg("\033[031mArgumentError: Argument {} in add instruction is not a register. ADD parameters are ADD regA regB regOut.\nValid prefixes for register argument: r $\n{}Line {} in file {}\n\033[01m".format(brokeninst[j], i, line, filename), 1)
+                compilesuccess = False
                 continue
             argconstructor = ""
             for k in range(1, len(brokeninst[j])):
@@ -131,13 +138,22 @@ for i in lines:
                 argconstructor = int(argconstructor)
             except ValueError:
                 error = True
-                msg("\033[031mArgumentError: Argument {} does not contain a valid number of type int in ADD instruction.\n{}\nLine {} in file {}\n\033[01m".format(brokeninst[j], i, line, filename), 1)
+                msg("\033[031mArgumentError: Argument {} does not contain a valid number of type int in ADD instruction.\n{}Line {} in file {}\n\033[01m".format(brokeninst[j], i, line, filename), 1)
                 continue
-            instructionbinary += d2b(argconstructor)
+            if argconstructor < 0:
+                argconstructor = -argconstructor
+                msg("Register values in arguments on line {} ({}) have been switched to positive numbers".format(line, i), 2)
+            if argconstructor > 16:
+                msg("Register values on line {} ({}) have been truncated to the bottom 4 bits.".format(line, i), 2)
+            instructionbinary += d2b(argconstructor, 4)
         instructionbinary = formatapp(instructionbinary, 18)
+        if compilesuccess:
+            msg("Succesful translation on line {}".format(line), 3)
+            msg("Instruction {} added to list.".format(instructionbinary), 4)
+        msg("Line was: {}".format(i), 4)
     else:
         error = True
-        msg("\033[031mOperationNotFoundError: Operation {} was not recognized.\n{}\nLine {} in file {}\n\033[01m".format(op.upper(), i, line, filename), 1)
+        msg("\033[031mOperationNotFoundError: Operation {} was not recognized.\n{}Line {} in file {}\n\033[01m".format(op.upper(), i, line, filename), 1)
         continue
     
 
