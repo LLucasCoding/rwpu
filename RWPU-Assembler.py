291509af8aca9e7c09670830c125b433d585b10f
Lucas <lukipuki.3.14159@gmail.com>
2025-03-22 09:53:11 -0600

    Added functions for checkline, possible use for simplified line parsing

--- RWPU-Assembler.py
+++ RWPU-Assembler.py
@@ -100,7 +100,148 @@ def log2int(integer): # log2(n) rounded down
         mask <<= 1
     return result
 
-if filename.split(".")[-1] != "rwpu" or len(filename.split(".")) == 1:
+def striparg(s): # Remove first character in a string
+    s = list(s)
+    out = ""
+    if (len(s) == 1):
+        return ""
+    for i in range(1, len(s)):
+        out += s[i]
+    return out
+
+def checkreg(s): # Returns (errcode, strippedval)
+    sl = list(s)
+    if sl[0] != "r" and sl[0] != "$":
+        return (1, 0) # Value is not register error
+    try:
+        arg = striparg(s)
+        arg = int(arg)
+    except ValueError:
+        return (2, 0) # Register reference is not valid
+    if arg < 0:
+        arg = -arg
+        arg = arg&15
+        return (3, arg) # Argument was negative (warn)
+    elif arg >= 16:
+        arg = arg&15
+        return (4, arg) # Argument was truncated (warn)
+    else:
+        return (0, arg) # Success
+
+def checklabel(s):
+    if list(s)[0] != ".":
+        return (1, 0) # womp womp, it's not a label
+    valid = False
+    global labelnames
+    for i in range(len(labelnames)):
+        if labelnames[i] == s:
+            return (0, labelpos[i]) # Success
+    return (2, 0) # Label not referenced / referenced later
+
+def checkaddr(s):
+    if list(s)[0] != "#":
+        return (1, 0) # not a valid inst address prefix
+    else:
+        s = striparg(s)
+        try:
+            s = int(s)
+            if s < 1:
+                s = -s
+                s &= 1023
+                return (3, 0) # Negative address (warn)
+            if s == 0:
+                return (4, 0) # Zero address, set to 1 (warn)
+            if s > 1023:
+                return (5, s&1023) # Address too high, truncated (warn)
+            return (0, s)
+        except ValueError:
+            return (2, 0) # Non-integer address
+
+def checkint(s):
+    try:
+        s = int(s)
+        if s < -128:
+            s = -s
+            s &= 255
+            return (2, s) # Integer below range
+        elif s > 255:
+            s &= 255
+            return (3, s) # Integer above range
+        elif s < 0:
+            s += 256
+        return (0, s)
+    except ValueError:
+        return (1, 0) # Not an integer
+        
+def checkline(broken, operands, linenum): # Operands: r: register, i: integer, a: absolute inst address, l: label
+    operands = list(operands)
+    values = []
+    line = ""
+    for i in broken:
+        line += i
+    global error
+
+    for i in range(1, len(operands)):
+        if operands[i] == "i": # Integer
+            checkout = checkint(broken[i])
+            if checkout[0] == 1:
+                msg("ValueError: Expecting integer, got {} instead.\n{}\nLine {} in input file.".format(broken[i], line, linenum), 1)
+                error += 1
+            elif checkout[0] == 2:
+                msg("Warning: Integer {} on line {} is below -128 (Below signed 8-bit integer limit)", 2)
+            elif checkout[0] == 3:
+                msg("Warning: Integer {} on line {} is above 255 (Above unsigned 8-bit integer limit)", 2)
+            else:
+                msg("Operand integer value good", 4)
+            values.append(checkout[1])
+        elif operands[i] == "r": # Register
+            checkout = checkreg(broken[i])
+            if checkout[0] == 1:
+                msg("ValueError: Expecting register, got {}. Valid register prefixes are r and $.\n{}\nLine {} in input file.".format(broken[i], line, linenum), 1)
+                error += 1
+            elif checkout[0] == 2:
+                msg("RegisterNotFoundError: Register {} does not exist.\n{}\nLine {} in input file.".format(broken[i], line, linenum), 1)
+                error += 1
+            elif checkout[0] == 3:
+                msg("Warning: Register {} on line {} was negative. It has been converted to a valid register ({})".format(broken[i], linenum, checkout[1]), 2)
+            elif checkout[0] == 4:
+                msg("Warning: Register {} on line {} was above 15. The value has been truncated to the bottom 4 bits. ({})".format(broken[i], line, linenum, checkout[1]), 2)
+            else:
+                msg("Operand register value good", 4)
+            values.append(checkout[1])
+        elif operands[i] == "l":
+            checkout = checklabel(broken[i])
+            if checkout[0] == 1:
+                msg("ValueError: Expecting label, got {}. Valid label prefix is '.'.\n{}\nLine {} in input file.".format(broken[i], line, linenum), 1)
+                error += 1
+            elif checkout[0] == 2:
+                msg("LabelNotFoundError: Label {} has not been created yet.\n{}\nLine {} in input file.".format(broken[i], line, linenum))
+            else:
+                msg("Operand label value good", 4)
+            values.append(checkout[1])
+        elif operands[i] == "a":
+            checkout = checkaddr(broken[i])
+            if checkout[0] == 1:
+                msg("ValueError: Expecting instruction address, got {}. Valid addr prefix is #.\n{}\nLine {} in input file.".format(broken[i], line, linenum), 1)
+                error += 1
+            elif checkout[0] == 2:
+                msg("AddressError: Given address {} is not a valid address. Please retry with an integer.\n{}\nLine {} in input file.".format(broken[i], line, linenum), 1)
+            elif checkout[0] == 3:
+                msg("Warning: Given instruction address {} on line {} was negative, it was switched to address {}.".format(broken[i], linenum, checkout[1]), 2)
+            elif checkout[0] == 4:
+                msg("Warning: Given instruction address on line {} was 0. Instruction line 0 is always a NOP.".format(linenum), 2)
+            elif checkout[0] == 5:
+                msg("Warning: Given instruction address {} on line {} was above 1023. It has been truncated to the bottom 10 bits. ({})".format(broken[i], linenum, checkout[1]))
+            else:
+                msg("Operand address value good", 4)
+            values.append(checkout[1])
+    if error == 0:
+        msg("Succesful compile, line {}".format(linenum), 3)
+    return values
+
+if filename.split(".")[-1] == "rwpumc":
+    msg("Warning: Filename specified ends in .rwpumc, which is the machine code file extension, not the assembly file extension.", 2)
+elif filename.split(".")[-1] != "rwpu" or len(filename.split(".")) == 1:
     msg("Warning: Filename specified doesn't end in .rwpu, which is the proper file extension", 2)
 
 if list(lines[-1])[-1] != "\n":
@@ -123,8 +264,14 @@ for i in lines:
     msg("Broken instruction: {}".format(brokeninst), 4)
     if list(i)[0] == ".": # if the line is a label
         msg("Label found on line", 4)
-        labelnames.append(lines.split(" ")[0])
-        labelpos.append(instruction)
+        validlabel = True
+        for j in labelnames:
+            if brokeninst[0] == j:
+                validlabel = False
+                msg("LabelError: Label {} has already been created.\n{}Line {} in input file".format(brokeninst[0], i, line))
+        if validlabel:
+            labelnames.append(lines.split(" ")[0])
+            labelpos.append(instruction)
         if len(brokeninst) == 1: # if the label is alone on it's line, continue to the next line
             msg("Label was the only thing found on that line", 4)
             continue
